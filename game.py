08672b33ae5116230b7714f6c276bf9fb1c4db58
Daniel Margulis <damargulis@wustl.edu>
2017-05-15 12:43:43 -0400

    player picking

--- game.py
+++ game.py
@@ -2,23 +2,21 @@ from player import *
 from board import Board
 import collections
 
+player_options = {
+    'HumanPlayer': HumanPlayer,
+    'ComputerPlayer': ComputerPlayer,
+    'MinimaxPlayer': MinimaxPlayer,
+}
+
 class Game(object):
     def __init__(self,options):
-        if(options.white == 'HumanPlayer'):
-            player1 = HumanPlayer(0,'w',-1)
-        elif(options.white == 'ComputerPlayer'):
-            player1 = ComputerPlayer(0,'w',-1)
-        elif(options.white == 'MinimaxPlayer'):
-            player1 = MinimaxPlayer(0,'w',-1,depth=options.depth)
+        if options.white in player_options:
+            player1 = player_options[options.white](0,'w',-1,depth=options.depth)
         else:
             print('White Player Agent not recognized (user control is default)')
             exit()
-        if(options.black == 'HumanPlayer'):
-            player2 = HumanPlayer(1,'b',1)
-        elif(options.black == 'ComputerPlayer'):
-            player2 = ComputerPlayer(1,'b',1)
-        elif(options.black == 'MinimaxPlayer'):
-            player2 = MinimaxPlayer(1,'b',1,depth=options.depth)
+        if options.black in player_options:
+            player2 = player_options[options.black](1,'b',1,depth=options.depth)
         else:
             print('Black Player Agent not recognized (computer control is default)')
             exit()
